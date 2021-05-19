e33c81f252ef90ae127a516b22ca9729e63d7473
hugo <HugoTrejo@cisco>
2021-05-19 11:52:37 -0700

    Initial commit

---
+++ abstract.py
@@ -0,0 +1,14 @@
+import abc
+
+class BluePrint(abc.ABC):
+    @abc.abstractmethod
+    def hello(self):
+        pass
+
+class GreenField(BluePrint):
+    def hello(self):
+        print('Welcome to Green Field!')
+gf = GreenField()
+gf.hello()
+
+bp = BluePrint()
